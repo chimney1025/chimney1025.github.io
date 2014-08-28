ee98efa9d9fae05554801a5f97a99a082ec755bc
chimney1025 <holiday1025@gmail.com>
2014-08-28 16:21:15 +0100

    Update animations.js

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,27 +1,57 @@
 var rapidScoreAnimations = angular.module('rapidScoreAnimations', ['ngAnimate']);
 
 
-rapidScoreAnimations.controller('ScoreTopCtrl', ['$scope', 'Score',
-  function($scope, Top) {
-    $scope.scores = Top.query({scoreId:'top'});
-    $scope.orderProp = 'age';
-	
-	$scope.currentIndex = 0;
-	
-	$scope.setCurrentSlideIndex = function (index) {
-		$scope.currentIndex = index;
-	};
-
-	$scope.isCurrentSlideIndex = function (index) {
-		return $scope.currentIndex === index;
-	};
-
-    $scope.prevSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex < $scope.scores.length - 1) ? ++$scope.currentIndex : 0;
-    };
-
-    $scope.nextSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.scores.length - 1;
-	};
-	
+rapidScoreAnimations.controller('SliderCtrl', ['$scope',
+  function($scope) {
+  	$scope.images=[{src:'img1.png',title:'Pic 1'},{src:'img2.png',title:'Pic 2'},{src:'img3.png',title:'Pic 3'},{src:'img4.png',title:'Pic 4'},{src:'img5.png',title:'Pic 5'}];
   }]);
+
+rapidScoreAnimations.directive('slider', function($timeout){
+	return {
+    restrict: 'AE',
+	replace: true,
+	scope:{
+		images: '='
+	},
+    link: function (scope, elem, attrs) {
+	
+		scope.currentIndex=0;
+
+		scope.next=function(){
+			scope.currentIndex<scope.images.length-1?scope.currentIndex++:scope.currentIndex=0;
+		};
+		
+		scope.prev=function(){
+			scope.currentIndex>0?scope.currentIndex--:scope.currentIndex=scope.images.length-1;
+		};
+		
+		scope.$watch('currentIndex',function(){
+			scope.images.forEach(function(image){
+				image.visible=false;
+			});
+			scope.images[scope.currentIndex].visible=true;
+		});
+		
+		/* Start: For Automatic slideshow*/
+		
+		var timer;
+		
+		var sliderFunc=function(){
+			timer=$timeout(function(){
+				scope.next();
+				timer=$timeout(sliderFunc,5000);
+			},5000);
+		};
+		
+		sliderFunc();
+		
+		scope.$on('$destroy',function(){
+			$timeout.cancel(timer);
+		});
+		
+		/* End : For Automatic slideshow*/
+		
+    },
+	templateUrl:'views/home.html'
+  }
+})
