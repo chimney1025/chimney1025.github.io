f37a4e5a6d82cbe7c4717643f339f98270727404
chimney1025 <holiday1025@gmail.com>
2014-09-10 16:07:52 +0100

    Update animations.js

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,15 +1,5 @@
 var rapidScoreAnimations = angular.module('rapidScoreAnimations', ['ngAnimate']);
 
-rapidScoreAnimations.directive('sliderimg', function(){
-    return {
-        restrict: 'AE',
-        replace: true,
-        link: function (scope, elem, attrs) {
-            console.log(elem[0].clientHeight);
-        }
-    }
-});
-
 rapidScoreAnimations.directive('slider', function($timeout, $q){
     return {
         restrict: 'AE',
@@ -31,6 +21,9 @@ rapidScoreAnimations.directive('slider', function($timeout, $q){
         link: function (scope, elem, attrs) {
             
             scope.$on("Data_Ready", function(){
+                
+                console.log(elem.children());
+                
                 scope.currentIndex=0;
 
                 scope.next=function(){
