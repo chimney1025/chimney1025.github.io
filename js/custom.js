34b9f6360bee63214a98944d5512d5b585d0e0f6
Haoli Qu <haoli.qu@arm.com>
2015-09-30 14:52:29 +0100

    added click event for contact page

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,9 @@ var FormSelect = (function(){
 
     var toggleShowHide = function(event) {
         var selection = event.target.getAttribute('data-select');
-        document.getElementById(selection).style.display == "block"
-        ? document.getElementById(selection).style.display = "none"
-        : document.getElementById(selection).style.display = "block";
+        (document.getElementById(selection).style.display == "block")
+        ? (document.getElementById(selection).style.display = "none")
+        : (document.getElementById(selection).style.display = "block");
     };
 
     var toggleSelection = function(event) {
@@ -13,11 +13,17 @@ var FormSelect = (function(){
         button.classList.contains('active')
             ? button.classList.remove('active')
             : button.classList.add('active');
-    }
+    };
+	
+	var hideOverlay = function(event, modals) {
+		console.log(event.target);
+	}
 
     return {
         init: function() {
             var items = document.querySelectorAll('input');
+			var modals = document.querySelectorAll('.selection');
+            var spans = document.querySelectorAll('.selection span');
 
             for(var i=0; i<items.length; i++) {
                 if(items[i].getAttribute('data-select')) {
@@ -25,10 +31,13 @@ var FormSelect = (function(){
                 }
             }
 
-            var spans = document.querySelectorAll('.selection span');
             for(var i=0; i<spans.length; i++) {
                 spans[i].addEventListener('click', toggleSelection);
             }
+			
+			document.addEventListener('click', function(e){
+				hideOverlay(e, modals);
+			});
         }
     }
 
