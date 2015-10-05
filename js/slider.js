81d02b3eaa5a9d0ffd1c6f328648854c6c9be891
Haoli Qu <haoli.qu@arm.com>
2015-10-05 12:53:08 +0100

    added slider to index page

--- js/slider.js
+++ js/slider.js
@@ -1,61 +1,41 @@
 var Slider = function(id) {
-	var id = id;
-    var images = document.querySelectorAll("#" + id + " .slide");
-    var holder = document.querySelector("#" + id + " .holder");
-    var slider = document.getElementById(id);
-    var width = slider.offsetWidth;
-    var ratio = slider.offsetWidth/slider.offsetHeight;
-    holder.style.width = images.length * 100 + '%';
-	holder.style.left = 0;
-
-    var slideToLeft = function() {
-        if(parseInt(holder.style.left) > -width * (images.length-1)) {
-            holder.style.left = parseInt(holder.style.left) - width + 'px';
-        }
-	};
-
-	var slideToRight = function() {
-        if(parseInt(holder.style.left) < 0) {
-            holder.style.left = parseInt(holder.style.left) + width + 'px';
-        }
-	};
-
-    var scroll = function() {
-        for(var i=0; i<images.length; i++) {
-            images[i].style.transform = 'translate3d(-' + (100 - holder.scrollLeft/6) + 'px,0,0)';
-        }
-    };
-
-    var resize = function() {
-        width = slider.offsetWidth;
-        slider.style.height = width/ratio + 'px';
-        console.log(slider.offsetWidth + ' ' + slider.offsetHeight);
-        for(var i=0; i<images.length; i++) {
-            images[i].style.width = slider.offsetWidth + 'px';
-            images[i].style.height = slider.offsetHeight + 'px';
-        }
-    };
+	var index = 0;
+	var slider = document.getElementById(id);
+	var allSlides = slider.querySelectorAll('.slide');
+	var number = allSlides.length;
+	
+	function showSlide(currentIndex) {
+		for(var i=0; i<number; i++) {
+			if(i != currentIndex) {
+				allSlides[i].style.display = "none";
+			} else {
+				allSlides[i].style.display = "block";
+			}
+			
+		}
+	}
+	
+	function showPrev() {
+		index <= 0 ? (index = number -1) : (index -= 1);
+		showSlide(index);
+		
+	}
+	
+	function showNext() {
+		index >= number -1 ? (index = 0) : (index += 1);
+		showSlide(index);
+	}
+	
   
 	return {
-		start : function() {
-            //resize();
-
-			var that = this;
-			document.querySelector('#' + id + ' .prev').addEventListener('click', function(){
-				slideToRight();
-			}, false);
-	
-			document.querySelector('#' + id + ' .next').addEventListener('click', function(){
-				slideToLeft();
-			}, false);
-
-            slider.addEventListener('scroll', function(){
-                scroll();
-            });
-
-            window.addEventListener('resize', function(){
-                //resize();
-            });
+		init : function() {
+			var prev = slider.querySelector('.prev');
+			var next = slider.querySelector('.next');
+			
+			prev.addEventListener('click', showPrev);
+			next.addEventListener('click', showNext);
+			
+			console.log(number);
 		}
 	}
 }
